be678d298e53f09b87832b89f4f1080eb0b98290
unknown <LD-TArs@.(none)>
2012-12-06 10:05:54 +0700

    pertemuan 9

---
+++ pertemuan 9/6/Person.java
@@ -0,0 +1,9 @@
+public class Person{
+	public static void main(String[] args) {
+		boolean test1;
+		boolean test2;
+
+		test1 = (5 != 3);
+		System.out.println(test1);
+	}
+}
---
+++ pertemuan 9/7/Person.java
@@ -0,0 +1,11 @@
+public class Person{
+	public static void main(String[] args) {
+		int firstNumber, secondNumber, results;
+
+		firstNumber = 20;
+		secondNumber = 10;
+
+		results = firstNumber + secondNumber;
+		System.out.println(results);
+	}
+}
